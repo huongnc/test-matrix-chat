ecce6cacad061609c47e2423359bb7f87e1b68dc
Chí Hưởng <chihuong28502@gmail.com>
2026-04-19 22:48:35 +0700

    docker save

--- backend/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  extensionsToTreatAsEsm: [".ts"],
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1"
-  }
-};
